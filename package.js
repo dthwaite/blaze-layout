28053c7a2a04e7876f77378e147ddb8c07ff2fe6
Dominic Thwaites <dominicthwaites@mac.com>
2024-11-06 12:52:09 +0000

    Update package.js

--- package.js
+++ package.js
@@ -21,11 +21,11 @@ Package.onTest(function(api) {
 
 function configure(api) {
   api.versionsFrom('1.0');
-  api.use('blaze@3.0.0');
+  api.use('blaze@3.0');
   api.use('templating');
   api.use('reactive-dict');
   api.use('underscore');
-  api.use('jquery');
+  api.use('jquery@3.0');
   api.use('tracker');
 
   api.addFiles('lib/client/namespace.js', 'client');
